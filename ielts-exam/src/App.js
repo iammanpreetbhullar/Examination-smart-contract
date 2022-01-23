23f9c49de9b425d0ed2bd3cc028e69563c00c181
manpreetsinghbhullar <mr.manpreetbhullar@gmail.com>
2022-01-23 19:25:30 +0530

    landing page update

--- ielts-exam/src/App.js
+++ ielts-exam/src/App.js
@@ -257,9 +257,10 @@ class App extends React.Component {
                         </div>
                     </Container>
                 </Navbar>
-                <div style={{ width: "100vw", height: "92vh" }}>
-                    <img src={img} height='400px' width='800px' style={{ marginTop: "130px", marginLeft: "150px", border: "3px solid white", borderRadius: "50px", boxShadow: "0px 0px 20px 0px white" }} />
-                    <div style={{ color: "white", float: "right", marginTop: "230px", marginRight: "70px", fontSize: "80px", width: "500px", textAlign: "center", fontWeight: "bold" }}>
+                <div className='d-flex align-items-center justify-content-around' style={{ height:"92vh" }}>
+                    <div><img src={img} height='300px' width='700px' style={{ border: "3px solid white", borderRadius: "50px", boxShadow: "0px 0px 20px 0px white" }} />
+                    </div>
+                    <div style={{ color: "white", width:"30%", fontSize: "60px", textAlign: "center", fontWeight: "bold" }}>
                         IELTS Online Exam
                     </div>
                 </div>
