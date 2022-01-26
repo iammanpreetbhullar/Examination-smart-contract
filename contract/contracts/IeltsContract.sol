847486329e13a1e2e946ba4f9044417c40eb1d25
manpreetsinghbhullar <mr.manpreetbhullar@gmail.com>
2022-01-26 19:51:38 +0530

    pre-final commit 2

--- contract/contracts/IeltsContract.sol
+++ contract/contracts/IeltsContract.sol
@@ -1,18 +1,42 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity >=0.8.0 < 0.9.0;
+pragma solidity >=0.8.0 <0.9.0;
 
 contract IeltsContract {
-
-   string[]  myString;
-
-    function getArray() public view returns (
-        string[] memory) {         
-            return myString;
-        }  
-
-    function push_array(string memory ans) public {      
-        myString.push(ans);            
+    struct Student {
+        string Id;
+        string Answers;
     }
 
+    Student student;
+
+    mapping(string => Student) public students;
+
+    function getStudentMarks(string memory _id)
+        public
+        view
+        returns (
+            string memory Id,
+            string memory Answers,
+            address sender
+        )
+    {
+        Student memory s = students[_id];
+        return (s.Id, s.Answers, msg.sender);
+    }
+
+    function addMarks(string memory Id, string memory Ans) public {
+        student = Student(Id, Ans);
+        // students[Id] =  student;
+    }
+
+    string[] myString;
+
+    function getArray() public view returns (string[] memory) {
+        return myString;
+    }
+
+    function push_array(string memory ans) public {
+        myString.push(ans);
+    }
 }
--- ielts-exam/src/config.js
+++ ielts-exam/src/config.js
@@ -1,30 +1,103 @@
-export const Cont_Addr = "0x5a0b4E2826A1B69E57882d85CA372EC8b6Ff35e9";
+export const Cont_Addr = "0x4BCA70a54bACC4b0d53a497305DE9D28861aB899";
 export const Cont_ABI = [
-  {
-    "inputs": [],
-    "name": "getArray",
-    "outputs": [
-      {
-        "internalType": "string[]",
-        "name": "",
-        "type": "string[]"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function",
-    "constant": true
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "string",
-        "name": "ans",
-        "type": "string"
-      }
-    ],
-    "name": "push_array",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }
-];
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "name": "students",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "Id",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "Answers",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_id",
+          "type": "string"
+        }
+      ],
+      "name": "getStudentMarks",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "Id",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "Answers",
+          "type": "string"
+        },
+        {
+          "internalType": "address",
+          "name": "sender",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "Id",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "Ans",
+          "type": "string"
+        }
+      ],
+      "name": "addMarks",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getArray",
+      "outputs": [
+        {
+          "internalType": "string[]",
+          "name": "",
+          "type": "string[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "ans",
+          "type": "string"
+        }
+      ],
+      "name": "push_array",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ];
